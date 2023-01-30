1e906d02a3b1e5f2665e04b0175d37905b14e270
mrohangithub <mrohan2005@gmail.com>
2023-01-29 19:33:48 -0800

    Jwt change

--- src/main/java/com/nighthawk/spring_portfolio/Main.java
+++ src/main/java/com/nighthawk/spring_portfolio/Main.java
@@ -3,12 +3,15 @@ package com.nighthawk.spring_portfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nighthawk.spring_portfolio.mvc.jwt.Databasemanager;
+
 // @SpringBootApplication annotation is key to building web applications with Java https://spring.io/projects/spring-boot
 @SpringBootApplication
 public class Main {
 
     // Starts a spring application as a stand-alone application from the main method
     public static void main(String[] args) {
+        Databasemanager.initializeDatabase();
         SpringApplication.run(Main.class, args);
     }
 
